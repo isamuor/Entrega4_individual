06cdf7b4abdfaed587578493d6795b2c087ed091
isamuor <isabelc.munoz@udea.edu.co>
2019-04-10 16:51:56 -0500

    cambio en config para que funcione mongo atlas

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 process.env.PORT = process.env.PORT || 3000;
-process.env.URLDB = 'mongodb://localhost:27017/Education';
+process.env.URLDB = process.env.URLDB || 'mongodb://localhost:27017/Education';
 
 
 /*process.env.NODE_ENV = process.env.NODE_ENV || 'local';
